957b9088f6ac4cfb801fd8beb9dd03b65916211e
Abhishek MD <92582070+AbhishekMD1998@users.noreply.github.com>
2026-05-21 09:16:32 +0530

    Testing

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,15 @@
-// lib/prisma.ts
+// // lib/prisma.ts
+// import { PrismaClient } from '@prisma/client';
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+//   });
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
@@ -6,7 +17,12 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
